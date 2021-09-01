19e48100d436c11e252a0a6030d2d956824ffd4b
n.povalyaeva <nataskapoval@mail.com>
2021-09-01 19:08:33 +0300

    Переделала реализацию бизнес логики на более читаемую и понятную и сразу с использованием switch для ДЗ

--- app/src/main/java/com/tinkoff/edu/app/controller/LoanCalcController.java
+++ app/src/main/java/com/tinkoff/edu/app/controller/LoanCalcController.java
@@ -2,10 +2,10 @@ package com.tinkoff.edu.app.controller;
 
 
 import com.tinkoff.edu.app.request.LoanRequest;
-import com.tinkoff.edu.app.request.LoanType;
 import com.tinkoff.edu.app.response.LoanResponse;
 import com.tinkoff.edu.app.response.ResponseType;
 import com.tinkoff.edu.app.service.BusinessService;
+
 
 public class LoanCalcController {
     private BusinessService service;
@@ -15,21 +15,28 @@ public class LoanCalcController {
         this.service = service;
     }
 
-    /**
-     * TODO Validates and logs request
-     */
     public LoanResponse createRequest(LoanRequest request) {
-
-
-        //param validation
-        //log request
-        //LoanCalcLogger.log(request);
-        if ((request != null && request.getType().equals(LoanType.PERSON) && (request.getAmount() > 0 && request.getAmount() <= 10000) && ((request.getMonths() > 0) && request.getMonths() <= 12)) || ((request != null && request.getType().equals(LoanType.OOO)) && (request.getAmount() > 10000) && (request.getMonths() > 0 && request.getMonths() < 12))) {
-            return new LoanResponse(ResponseType.APPROVED, service.createRequest(request));
-        } else if (request == null || request.getAmount() <= 0 || request.getMonths() <= 0) {
+        if (request != null && request.getMonths() > 0 && request.getAmount() > 0) {
+            switch (request.getType()) {
+                case PERSON:
+                    if (request.getAmount() <= 10000 && request.getMonths() <= 12) {
+                        return new LoanResponse(ResponseType.APPROVED, service.createRequest(request));
+                    } else {
+                        return new LoanResponse(ResponseType.DENIED, service.createRequest(request));
+                    }
+                case OOO:
+                    if (request.getAmount() > 10000 && request.getMonths() < 12) {
+                        return new LoanResponse(ResponseType.APPROVED, service.createRequest(request));
+                    } else {
+                        return new LoanResponse(ResponseType.DENIED, service.createRequest(request));
+                    }
+                case IP:
+                    return new LoanResponse(ResponseType.DENIED, service.createRequest(request));
+                default:
+                    return new LoanResponse(ResponseType.ERROR, -1);
+            }
+        } else {
             return new LoanResponse(ResponseType.ERROR, -1);
-        } else
-            return new LoanResponse(ResponseType.DENIED, service.createRequest(request));
+        }
     }
 }
-
